82c9fe3184a2911510c2db73b7e8caa6414cd80e
tedmist1 <tyler.k.edmiston@gmail.com>
2025-11-07 13:36:54 -0800

    Changed to email(passwordless setup) as default login

--- terraform/auth0_tenant/connections.tf
+++ terraform/auth0_tenant/connections.tf
@@ -4,22 +4,24 @@ resource "auth0_connection" "passwordless-email" {
     name = "email"
     
     options {
-    name                     = "email"
-    from                     = "{{ application.name }} \u003croot@auth0.com\u003e"
-    subject                  = "Welcome to {{ application.name }}"
-    syntax                   = "liquid"
-    template                 = "<html>Please login using this passwordless email.</html>"
-    disable_signup           = false
-    brute_force_protection   = true
-    non_persistent_attrs     = []
-    auth_params = {
-      scope         = "openid email profile offline_access"
-      response_type = "code"
-    }
+        name                     = "email"
+        from                     = "{{ application.name }} \u003croot@auth0.com\u003e"
+        subject                  = "Welcome to {{ application.name }}"
+        syntax                   = "liquid"
+        template                 = "<html>Please login using this passwordless email.</html>"
+        disable_signup           = false
+        brute_force_protection   = true
+        non_persistent_attrs     = []
+        auth_params = {
+        scope         = "openid email profile offline_access"
+        response_type = "code"
+        }
 
-    totp {
-      time_step = 300
-      length    = 6
+        totp {
+        time_step = 300
+        length    = 6
+        }
+
+        password_policy = "excellent"
     }
-  }
 }
--- terraform/auth0_tenant/tenant.tf
+++ terraform/auth0_tenant/tenant.tf
@@ -3,15 +3,14 @@ resource "auth0_tenant" "tenant"{
 
     support_email = var.support_email
     support_url = var.support_url
-    default_directory = "Username-Password-Authentication" # TODO Lookup more details on this
-    sandbox_version = "22" # TODO lookup most up to date version. Might want to throw this in a variable though and just have a default value
+    default_directory = "email" # Sets the default login type to email, which matches the passwordless setup we're using.
+    sandbox_version = "22" # 22 is the most up to date version
 
-    flags { # TODO lookup more flags and verify these should be set.
-        # universal_login = true  
+    # Flags could be further explored, potentially more settings that would be good to enable
+    flags { 
         enable_pipeline2 = true
         enable_apis_section = true
         enable_client_connections = false
-        # change_password_flow_v1 = false
         mfa_show_factor_list_on_enrollment = true
     }
 
@@ -19,8 +18,7 @@ resource "auth0_tenant" "tenant"{
     idle_session_lifetime = var.idle_session_lifetime
 
     session_cookie {
-        mode = "non-persistent" # TODO potentially modify
+        mode = "non-persistent" 
     }
-
     
 }
